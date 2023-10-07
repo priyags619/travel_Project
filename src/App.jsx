f4c5584d91f95ef375a648989deb49eaade596a8
priyags <priyags619@gmail.com>
2023-10-07 14:51:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Destinations from './components/Destinations/Destinations'
+import Navbar from './components/Navbar/Navbar'
+import 'bootstrap/dist/css/bootstrap.css'
+
+function App() {
+
+  return (
+    
+      <div>
+       <Destinations/>
+       <Navbar/>
+    </div>
+  )
+}
+
+export default App
